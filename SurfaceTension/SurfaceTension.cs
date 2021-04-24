bc159eaf03e7f8e6069f81deae791aa066df4758
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-04-24 12:22:03 -0700

    add funni info, build with 2.8.0

--- SurfaceTension/SurfaceTension.cs
+++ SurfaceTension/SurfaceTension.cs
@@ -1,11 +1,19 @@
 ﻿namespace SurfaceTension
 {
+    using System;
     using Exiled.API.Features;
     using ServerHandlers = Exiled.Events.Handlers.Server;
     using WarheadHandlers = Exiled.Events.Handlers.Warhead;
 
     public class SurfaceTension : Plugin<Config>
     {
+
+        public override string Author { get; } = "BuildBoy12";
+        public override string Name { get; } = "Surface Tension";
+        public override string Prefix { get; } = "ST";
+        public override Version Version { get; } = new Version(1, 3, 3);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 29, 0);
+
         private EventHandlers _eventHandlers;
 
         public override void OnEnabled()
