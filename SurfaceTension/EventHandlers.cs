73f532b4b23685fad30d1a87132aa153e61bcd52
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-07-22 17:24:59 -0700

    Update exiled, fix small bug

--- SurfaceTension/EventHandlers.cs
+++ SurfaceTension/EventHandlers.cs
@@ -1,9 +1,7 @@
 namespace SurfaceTension
 {
     using Exiled.API.Features;
-    using Exiled.Events.EventArgs;
     using MEC;
-    using System.Collections.Generic;
 
     public class EventHandlers
     {
@@ -15,47 +13,57 @@ namespace SurfaceTension
             return isPercent ? playerMaxHp / 100 * damage : damage;
         }
 
-        private IEnumerator<float> DamageOverTime()
+        private void DamageOverTime()
         {
             if (_config.DelayTime > 0)
-                yield return Timing.WaitForSeconds(_config.DelayTime);
-
-                //Cassie warning
-                if (_config.EnableCassie == true)
             {
-                Cassie.Message(_config.CassieMessage);
-            }
-
-                //Warning broadcast
-                if (_config.EnableBroadcast == true)
-            {
-                Map.Broadcast(duration: _config.BroadcastDuration, message: _config.BroadcastMessage);
-            }
-                //Console logs for tension
-                if (_config.ConsoleLogs == true)
-            {
-                if (_config.DamageAsPercentage == true) { Log.Info($"Surface Tension has started! Now damaging players for {_config.DamageAmount}% of their health every {_config.DamageInterval} second(s)."); }
-                else { Log.Info($"Surface Tension has started! Now damaging players for {_config.DamageAmount} HP every {_config.DamageInterval} second(s)."); }
-                
-            }
-                
-            while (Warhead.IsDetonated)
-            { 
-                yield return Timing.WaitForSeconds(_config.DamageInterval > 0 ? _config.DamageInterval : 0.1f);
-                foreach (Player ply in Player.List)
+                Timing.CallDelayed(_config.DelayTime, () =>
                 {
-                    if (!ply.IsAlive)
-                        continue;
+                    if (!Warhead.IsDetonated)
+                        return;
+                    
+                    //Cassie warning
+                    if (_config.EnableCassie == true)
+                    {
+                        Cassie.Message(_config.CassieMessage);
+                    }
 
-                    ply.Hurt(DamageCalculation(_config.DamageAsPercentage, ply.MaxHealth, _config.DamageAmount));
-                    ply.ShowHint(duration: _config.DamageInterval, message: $"{_config.DamageMessage}");
-                }
+                    //Warning broadcast
+                    if (_config.EnableBroadcast == true)
+                    {
+                        Map.Broadcast(duration: _config.BroadcastDuration, message: _config.BroadcastMessage);
+                    }
+                    //Console logs for tension
+                    if (_config.ConsoleLogs == true)
+                    {
+                        if (_config.DamageAsPercentage == true) { Log.Info($"Surface Tension has started! Now damaging players for {_config.DamageAmount}% of their health every {_config.DamageInterval} second(s)."); }
+                        else { Log.Info($"Surface Tension has started! Now damaging players for {_config.DamageAmount} HP every {_config.DamageInterval} second(s)."); }
+                
+                    }
+                
+                    while (Warhead.IsDetonated)
+                    {
+                        Timing.CallDelayed(_config.DamageInterval > 0 ? _config.DamageInterval : 0.1f, () =>
+                        {
+                            foreach (Player ply in Player.List)
+                            {
+                                if (!ply.IsAlive)
+                                    continue;
+
+                                ply.Hurt(DamageCalculation(_config.DamageAsPercentage, ply.MaxHealth, _config.DamageAmount));
+                                ply.ShowHint(duration: _config.DamageInterval, message: $"{_config.DamageMessage}");
+                            }
+                        });
+                    }
+                });
             }
+            
         }
 
         public void OnWarheadDetonation()
         {
-            Timing.RunCoroutine(DamageOverTime());
+            DamageOverTime();
+
             if (_config.ConsoleLogs == true)
             {
                 Log.Info($"Warhead detonated. Surface Tension starting in {_config.DelayTime} seconds!");
